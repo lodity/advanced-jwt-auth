54e167cf83c1f2db6beebf13ce25c46551e006de
lodity <lodity9@gmail.com>
2023-06-07 15:22:08 +0300

    add router and user-controller

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import router from './router/index.js';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
 	try {
