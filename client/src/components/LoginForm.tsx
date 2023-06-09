8ac9834d218d60531eef5cf7e7ae06ae466e4286
lodity <lodity9@gmail.com>
2023-06-09 09:55:12 +0300

    add a working login/registration form

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,31 @@
+import React, { FC, useContext, useState } from 'react';
+import { Context } from '../index';
+
+const LoginForm: FC = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const { store } = useContext(Context);
+
+	return (
+		<div>
+			<input
+				onChange={(e) => setEmail(e.target.value)}
+				value={email}
+				type="text"
+				placeholder="Email"
+			/>
+			<input
+				onChange={(e) => setPassword(e.target.value)}
+				value={password}
+				type="password"
+				placeholder="Password"
+			/>
+			<button onClick={() => store.login(email, password)}>Login</button>
+			<button onClick={() => store.registration(email, password)}>
+				Registration
+			</button>
+		</div>
+	);
+};
+
+export default LoginForm;
